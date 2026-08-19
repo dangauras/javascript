e398903f1a70aeb4ce1ec861bf009f45b6334a75
dangauras <dangauras@gmail.com>
2026-08-18 18:33:47 -0700

    delete and add some files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 122345
+let accountEmail = "dangauras@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dhangadhi"
+
+accountId = 2
+
+ console.log(accountId);
+  
+//  console.log();
+ 
