e0480c0890fea9462e0223511e34534d2975a1d7
dangauras <dangauras@gmail.com>
2026-08-19 16:02:12 +0545

    data types learning

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,10 +2,16 @@ const accountId = 122345
 let accountEmail = "dangauras@gmail.com"
 var accountPassword = "12345"
 accountCity = "Dhangadhi"
+let accountState;
 
-accountId = 2
+// accountId = 2 // constant value not allowed to change
+//  using the let and var we can change the value of variable
 
  console.log(accountId);
   
 //  console.log();
+console.table({accountId, accountEmail, accountPassword, accountCity, accountState});
  
+/*
+Prefer not to use var,because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello world") // We are using nodejs, not browser, so alert will not work
+
+console.log(3 + 3); // we can print in console
+
+
+console.log("Suresh Dangaura");
+    
