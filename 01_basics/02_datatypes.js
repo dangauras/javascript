4a282a7458e7f827b4aa613ca48ee364e25144f2
dangauras <dangauras@gmail.com>
2026-08-19 17:09:54 +0545

    conversion operation learning

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,8 +2,32 @@
 
 //alert("hello world") // We are using nodejs, not browser, so alert will not work
 
-console.log(3 + 3); // we can print in console
+// console.log(3 + 3); // we can print in console
 
 
-console.log("Suresh Dangaura");
-    
+// console.log("Suresh Dangaura");
+
+let name = "suresh dangaura";
+let age = 36;
+let isLoggedIn = false;
+let state; 
+
+// number, string, boolean, null, undefined 
+
+// number=> 2 to popwer 53
+//bigint
+// string=> ""
+//boolean=> true/false
+// null=> standalone value
+// undefined=> value not assigned 
+// symbol=>  unique
+
+// object=> key value pair
+
+// console.log(typeof "suresh dangaura"); //string
+// console.log(typeof 36); // number
+// console.log(typeof false); //boolean
+console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof Symbol('id')); // symbol
+console.log(typeof {});    // object
